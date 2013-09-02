66cbeaf9b1b2ba11cb1ea4597dbbc8a8862f9fea
TheDarkSpoon <jtpushmail@gmail.com>
2013-09-02 18:23:17 +0100

    Removed Data File

--- Data/PQR_DarkSpoon_Data.lua
+++
@@ -1,7 +0,0 @@
-if not PQR_LoadedDataFile then
-	PQR_LoadedDataFile = 1
-	PQR_WriteToChat("|cffFFDB3DBroseph's Holy Paladin Data: Loaded!|cffffffff")
-end
-BUFF_BOK = 20217
-BUFF_BOM = 19740
-BUFF_SOI = 20165
